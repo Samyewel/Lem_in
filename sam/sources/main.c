91fc9b69150652c7f1b18547083ecc0ed3187a0c
Samuel Williams <swilliam@c1r4p11.hive.fi>
2022-11-15 17:29:47 +0200

    pushing to update sam dir

--- sam/sources/main.c
+++ sam/sources/main.c
@@ -6,21 +6,26 @@
 /*   By: swilliam <swilliam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 16:16:16 by swilliam          #+#    #+#             */
-/*   Updated: 2022/11/07 17:08:25 by swilliam         ###   ########.fr       */
+/*   Updated: 2022/11/15 17:29:29 by swilliam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
 
-int	error_management(char **input)
+int	error_management(char *input)
 {
-	int	i;
+	char	*line;
+	int		fd;
+	int		i;
 
+	line = NULL;
 	i = 1;
-	while (input[i])
+	fd = open(input[1], O_RDONLY);
+	while (i > 0)
 	{
-
-		i++;
+		i = get_next_line(fd, &line);
+		ft_printf("%s\n", line);
+		ft_strdel(&line);
 	}
 	return (1);
 }
@@ -36,11 +41,9 @@ int	main(int argc, char **argv)
 	if (!rooms || !links)
 		exit(EXIT_FAILURE);
 	i = 1;
-	if (argc > 1)
+	if (argc == 2)
 	{
-		while (i != argc)
-			ft_printf("input :%s\n", argv[i++]);
-		if (!error_management(argv))
+		if (!error_management(argv[1]))
 			exit(EXIT_FAILURE);
 		else
 		{
