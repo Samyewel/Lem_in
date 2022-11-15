12d256f7506345f697b4a804b4cb5d4809b3647d
Elliot Galibert <elliotgalibert@Elliot-MacBook-Pro.local>
2022-11-15 17:30:30 +0200

    added read ants.c file

---
+++ elliot/read_ants.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   read_ants.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: egaliber <egaliber@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/15 16:22:40 by egaliber          #+#    #+#             */
+/*   Updated: 2022/11/15 17:23:52 by egaliber         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/lem_in.h"
+
+void read_ants(t_data *data)
+{
+	char *line;
+	int ant_count;
+	int i;
+	
+	ant_count = 0;
+	line = NULL;
+	i = 0;
+	
+	get_next_line(0, *line);
+	while (line != '\0')
+	{
+		if (!line)
+			error_management();
+		if (line[i] == '0' && line[i] == '-')
+			error_management();
+		if (line[i] <= 47 && line[i] >= 58)
+			error_management();
+		i++;
+	}
+	ant_count = ft_atoi(line);
+	data->ant_count = ant_count;
+}
