7b4fd74ef24d3e6ef75cd93bcd5f90c68d411cbe
Elliot Galibert <egaliber@c1r4p9.hive.fi>
2022-11-16 13:29:49 +0200

    added handle_comments

--- elliot/sources/read_rooms.c
+++ elliot/sources/read_rooms.c
@@ -6,11 +6,21 @@
 /*   By: egaliber <egaliber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 12:29:58 by egaliber          #+#    #+#             */
-/*   Updated: 2022/11/16 12:46:50 by egaliber         ###   ########.fr       */
+/*   Updated: 2022/11/16 13:22:23 by egaliber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/lem_in.h"
+
+void	handle_comments(t_rooms *rooms, char *line)
+{
+	char *room_data;
+
+	room_data = NULL;
+	if (line[0] == '#' && line[1] != '#') // valid comment?
+		return ;
+	
+}
 
 void	read_rooms(t_rooms *rooms)
 {
@@ -19,7 +29,7 @@ void	read_rooms(t_rooms *rooms)
 	line = NULL;
 	while (get_next_line(0, &line))
 	{
-		if (line == '#')
-			handle_comments()
+		if (*line == '#')
+			handle_comments(rooms, line);
 	}
 }
