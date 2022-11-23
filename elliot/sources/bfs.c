46d6ab0897fe65d7edabef00bda14ee70751f249
Elliot Galibert <egaliber@c1r5p19.hive.fi>
2022-11-23 13:10:10 +0200

    small fixes on bfs

--- elliot/sources/bfs.c
+++ elliot/sources/bfs.c
@@ -6,7 +6,7 @@
 /*   By: egaliber <egaliber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/22 17:27:42 by egaliber          #+#    #+#             */
-/*   Updated: 2022/11/22 17:51:10 by egaliber         ###   ########.fr       */
+/*   Updated: 2022/11/23 13:09:48 by egaliber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,19 @@
 
 typedef struct s_queue
 {
-	struct s_rooms	*room;
+	struct s_rooms	*name;
 	struct s_queue	*next;
 }	t_queue;
 
-void	init_bfs(t_data *data, t_queue **head, t_queue **tail)
+void	init_bfs(t_rooms *rooms, t_queue **head, t_queue **tail, t_queue **que)
 {
 	*head = (t_queue *)malloc(sizeof(t_queue));
 	if (!*head)
 		error();
-	(*head)->room = room->start;
+	(*head)->room = rooms->start;
 	(*head)->next = NULL;
 	*tail = *head;
+	*que = *head;
 }
 
 static int	bfs(t_data *data, t_queue **head, t_rooms *rooms)
@@ -35,7 +36,7 @@ static int	bfs(t_data *data, t_queue **head, t_rooms *rooms)
 
 	que = NULL;
 	tail = NULL;
-	bfs_init(data, head, &tail, &que);
+	bfs_init(rooms, head, &tail, &que);
 	while (rooms->end->parent == NULL) // while end doesnthave (we havent checked all)
 	{
 		check_links()
