f5939a9129e66640e32cfb6af3485c774a4873d2
Samuel Williams <swilliam@c1r5p21.hive.fi>
2022-11-23 14:03:16 +0200

    swapped header file

--- includes/lem_in.h
+++ includes/lem_in.h
@@ -6,7 +6,7 @@
 /*   By: swilliam <swilliam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 16:28:47 by swilliam          #+#    #+#             */
-/*   Updated: 2022/11/07 17:02:20 by swilliam         ###   ########.fr       */
+/*   Updated: 2022/11/22 15:38:18 by swilliam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,55 @@
 # define LEM_IN_H
 
 # include "ft_printf.h"
+# include "get_next_line.h"
 # include "libft.h"
+# include <stdbool.h>
+
+typedef struct data
+{
+	int				ant_count;
+	bool			starting_search;
+	bool			ending_search;
+}				t_data;
+
+typedef struct links
+{
+	char			*name;
+	struct links	*next;
+}				t_links;
 
 typedef struct rooms
 {
 	char			*name;
 	int				coord_x;
 	int				coord_y;
-	int				start;
-	int				end;
+	bool			start;
+	bool			end;
+	int				ants;
 	struct rooms	*next;
-	struct rooms	*previous;
+	struct links	*links;
 }				t_rooms;
 
-typedef struct	links
-{
-	char			*a;
-	char			*b;
-	struct links	*next;
-	struct links	*previous;
-}				t_links;
+/*
+** Testing functions:
+*/
+void	print_data(t_data *data, t_rooms *rooms);
+
+/*
+** Initialisation:
+*/
+t_data	*initialise_data(t_data *data);
+
+/*
+** List utilities:
+*/
+t_rooms	*create_room(t_rooms *rooms);
+t_links	*add_link(t_rooms **rooms, char *link_a, char *link_b);
+
+/*
+** Reading:
+*/
+void	read_input(t_data *data, t_rooms **rooms);
+t_rooms	*store_room_data(t_data *data, t_rooms *rooms, char *line);
 
 #endif
