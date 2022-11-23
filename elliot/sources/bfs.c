720a0cd1fce5415df98f7bb2aff98e69e82d30dd
Elliot Galibert <egaliber@c1r5p19.hive.fi>
2022-11-23 13:50:31 +0200

    some changes

--- elliot/sources/bfs.c
+++ elliot/sources/bfs.c
@@ -6,11 +6,24 @@
 /*   By: egaliber <egaliber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/22 17:27:42 by egaliber          #+#    #+#             */
-/*   Updated: 2022/11/23 13:15:46 by egaliber         ###   ########.fr       */
+/*   Updated: 2022/11/23 13:48:36 by egaliber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
+
+typedef struct rooms
+{
+	char			*name;
+	int				coord_x;
+	int				coord_y;
+	bool			start;
+	bool			end;
+	int				ants;
+	struct rooms	*next;
+	struct rooms	*flow;
+	struct links	*links;
+}				t_rooms;
 
 typedef struct s_queue
 {
@@ -18,13 +31,23 @@ typedef struct s_queue
 	struct s_queue	*next;
 }	t_queue;
 
+void	check_links(t_queue **tail, t_queue *que)
+{
+	/*array of the links is needed
+	need to check the flows
+	*/
+	int i;
+
+	i = 0;
+}
+
 void	init_bfs(t_rooms *rooms, t_queue **head, t_queue **tail, t_queue **que)
 {
 	*head = (t_queue *)malloc(sizeof(t_queue));
 	if (!*head)
 		error();
-	(*head)->room = rooms->start;
-	(*head)->next = NULL;
+	*head->name = rooms->start;
+	*head->next = NULL;
 	*tail = *head;
 	*que = *head;
 }
@@ -39,6 +62,7 @@ static int	bfs(t_data *data, t_rooms *rooms)
 	tail = NULL;
 	head = NULL;
 	bfs_init(rooms, head, &tail, &que);
+	/*does end have a name/room assigned to it*/
 	while (rooms->end->parent == NULL) // while end doesnthave (we havent checked all)
 	{
 		check_links()
