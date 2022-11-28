9463e00948a723dec55e08ddc7d9c27a7edd71d8
Samyewel <77693164+Samyewel@users.noreply.github.com>
2022-11-28 17:24:59 +0200

    modified bfs link loop to add to end of list

--- sources/bfs.c
+++ sources/bfs.c
@@ -6,7 +6,7 @@
 /*   By: sam <sam@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/28 13:51:24 by sam               #+#    #+#             */
-/*   Updated: 2022/11/28 16:51:35 by sam              ###   ########.fr       */
+/*   Updated: 2022/11/28 17:23:48 by sam              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,30 @@
 bool	bfs(t_rooms **rooms)
 {
 	t_queue	*queue;
-	t_rooms	*temp;
+	t_rooms	*temp_room;
 	t_links *link;
+	t_queue	*temp_queue;
 
 	queue = NULL;
-	temp = find_start_room(rooms);
-	queue = create_queue_node(queue, temp->name);
-	//queue = add_to_queue(queue, temp->name);
+	temp_queue = NULL;
+	temp_room = find_start_room(rooms);
+	queue = create_queue_node(queue, temp_room->name);
+	//queue = add_to_queue(queue, temp_room->name);
 	link = NULL;
-	if (temp->links == NULL)
+	if (temp_room->links == NULL)
 		ft_printf_strerror("No links from start");
 	/*does end have a name/room assigned to it*/
 	while (!is_empty(&queue)) // while end doesnthave (we havent checked all)
 	{
-		if (temp->links != NULL)
+		if (temp_room->links != NULL)
 		{
-			link = temp->links;
+			link = temp_room->links;
 			while (link)
 			{
-				queue->next = create_queue_node(queue, link->name);
+				temp_queue = queue;
+				while (temp_queue->next != NULL)
+					temp_queue = temp_queue->next;
+				temp_queue->next = create_queue_node(queue, link->name);
 				//queue->next = add_to_queue(queue, link->name);
 				link = link->next;
 			}
@@ -41,7 +46,7 @@ bool	bfs(t_rooms **rooms)
 		queue->visited = true;
 		if (queue->next != NULL)
 			queue = queue->next;
-		temp = visit_next(&queue, rooms);
+		temp_room = visit_next(&queue, rooms);
 	}
 	return (is_empty(&queue));
 }
